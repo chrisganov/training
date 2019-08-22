22fb220586512b11fae2d502b4d3be8854022707
Oci Ganov <ociganov@yahoo.com>
2019-08-22 22:00:03 +0300

    Feedback

--- daniel/Week3-task(1-4)/task1.js
+++ daniel/Week3-task(1-4)/task1.js
@@ -1,11 +1,12 @@
 let kelvin = 5;
 let celsius = kelvin - 273;
-let farenheit = (celsius * (9/5))+32;
-let newton = (celsius *(33/100));
+let farenheit = (celsius * (9/5))+32; // TODO: Something is missing - read the task again
+let newton = (celsius *(33/100));  // TODO: Something is missing - read the task again
 let sentence = "The temperature in kelvins is "+ kelvin +" and its equal to " + celsius + "C";
 console.log(sentence); 
  sentence = "The temperature in kelvins is "+ kelvin +" and its equal to " + farenheit + "F";
 console.log(sentence); 
  sentence = "The temperature in kelvins is ".concat(kelvin).concat(" and its equal to ").concat(newton).concat("N");
 console.log(sentence); 
-// Има разминавания в стотни в по-малките числа и не мога да разбера защо, предполагам съм схванал идеята на упражнението
+
+// TODO: You have no used Template Strings, re-write your sentances in template strings
---
+++ daniel/Week3-task(1-4)/task2.js
@@ -0,0 +1,16 @@
+const humanYears = 17;
+let earlyYears = 2;
+earlyYears = 2 * 10.5;
+let laterYears = (humanYears-2) * 4; // TODO: Either read the assignment and follow it, or if you are going to do it your own way, make sure it's correct - do we really need "let" here ? 
+myDogYears = earlyYears + laterYears;
+name = "daniel";
+
+// const capitalize = (s) => {
+//     if (typeof s !== 'string') return ''
+//     return s.charAt(0).toUpperCase() + s.slice(1)
+//   }
+
+// TODO: Above is useless, use building methods for strings - google it.
+
+// console.log("My name is "+ capitalize('daniel') +". I am "+ humanYears +" years old in human years which is " + myDogYears + " years old in dog years.");
+// TODO: use template string
