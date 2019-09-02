3b72170b14386180cb80c42915d182a3cbcf6045
Alexander Savov <sunk0@abv.bg>
2019-09-02 15:33:18 +0300

    v1.00

--- alexander/week4/task-one.js
+++ alexander/week4/task-one.js
@@ -1,12 +1,6 @@
-// /**
-//  * @param {string} input - you don't need this
-//  */
 
-  //TODO:  if you want to make sure that it's a string, you can add a conditional like this:
-  // if (typeof inpyt === 'string') ... 
-
-getUserChoice = input => { // Where is your declaration ? 
-  //TODO:  what if I add "kan[FOKASNDFOASF" - what will it return? Note: In the task you must indicate what are the user's choices in order to play, not what he has entered 
+const getUserChoice = input => { 
+  if(typeof input ==='string'){
     let inputLower = input.toLowerCase();
     if(inputLower === "rock"){
       return "rock";
@@ -16,47 +10,57 @@ getUserChoice = input => { // Where is your declaration ?
     else if(inputLower === "scissors"){
       return "scissors";
     }
-    return inputLower;
+    else{
+      return inputLower;
+    }
+  }else{
+    return "TYPE STRING!!!"
+  }
 }
 
-// let userChoice = getUserChoice("PAPER"); Why do you need this ?
+// END OF USER CHOICE
 
-// END OF USER CHOICE - hint
-
-getComputerChoice = () => {
-  // fix your indent  
-let randomNumber = Math.round(Math.random() * 2); // You were right about this one
-if(randomNumber === 0){
-  return "rock";
-}
-else if(randomNumber === 1){
-  return "paper";
-}
-else if(randomNumber ===2){
-  return "scissors";
-}
+const getComputerChoice = () => {
+     let randomNumber = Math.round(Math.random() * 2);
+     if(randomNumber === 0){
+       return "rock";
+     }
+     else if(randomNumber === 1){
+        return "paper";
+     }
+      else if(randomNumber ===2){
+       return "scissors";
+    }
 }
 
-// let computerChoice = getComputerChoice(); Why do you need this?
 // END OF COMPUTER CHOICE
-determineWinner = (userChoice, computerChoice) => { // Where is your declaration?
-    if(userChoice === computerChoice){
-      return "Game is a tie!";
-    }
-    else if(userChoice === "rock" && computerChoice === "scissors" || userChoice === "paper" && computerChoice === "rock" || userChoice === "scissors" && computerChoice === "paper"){
-      return "User Won."
-    }else if(computerChoice === "rock" && userChoice === "scissors" || computerChoice === "paper" && userChoice === "rock" || computerChoice === "scissors" && userChoice === "paper"){
-      return "Computer Won."
+
+const determineWinner = (userChoice,computerChoice) => {
+  if (userChoice === computerChoice){
+    return "Tie!";
+    }else if (userChoice === 'paper' && computerChoice === 'rock'){
+      return"User win!";
+    }else if (userChoice === 'rock' && computerChoice === 'scissors'){
+      return"User win!";
+    }else if (userChoice === 'scissors' && computerChoice === 'rock'){
+      return"Computer win!";
+    }else if (userChoice === 'rock' && computerChoice === 'paper'){
+      return"Computer win!";
+    }else if (userChoice === 'paper' && computerChoice === 'scissors'){
+      return"Computer win!";
+    }else if (userChoice === 'scissors' || computerChoice === 'paper'){
+      return"User win!";
+    }else{
+      return("Invalid input, please try again");
     }
 }
-
-playGame = input => { // Where is your declaration?
-  // fix your indent
-let userChoice = getUserChoice(input); // Why let? Do you reassign the variable somewhere?
-let computerChoice = getComputerChoice(); // Same question as above
-console.log(`User threw ${userChoice}`);
-console.log(`Computer threw ${computerChoice}`);
-console.log(determineWinner()); //TODO:  you are missing something here - look at your function
+// END OF DETERMINEWINNER
+const playGame = input => {
+     const userChoice = getUserChoice(input); 
+     const computerChoice = getComputerChoice();
+     console.log(`User threw ${userChoice}`);
+     console.log(`Computer threw ${computerChoice}`);
+     console.log(determineWinner(userChoice,computerChoice));
 }
 
-playGame("rock");
+playGame("paper");
--- alexander/week4/task-three.js
+++ alexander/week4/task-three.js
@@ -1,21 +1,16 @@
 let input = 'samurai life above all';
 let vowels = ["a", "e", "i", "o", "u"];
 let resultArray = [];
-// let inputIndex = 0; You don't need this
-// let vowelIndex = 0; You don't need this
-// TODO: Read about the "for" loop a bit more - The task says "while", but that doesn't mean you have to use "while" loop
-//TODO:  fix your indents within the functions
-while(inputIndex < input.length){ // TODO: wrong type of loop - use "for" loop
-inputIndex++;
-while(vowelIndex < vowels.length){ // TODO: again, wrong type of loop
-vowelIndex++;
-if(input[inputIndex] === vowels[vowelIndex]){
-    resultArray.push(input[inputIndex])
-}
-if(input[inputIndex] === "e" || "u"){
-    resultArray.push(input[inputIndex])
-}
-}
+
+for(let inputIndex = 0; inputIndex < input.length; inputIndex++){
+    for(let vowelIndex = 0; vowelIndex < vowels.length; vowelIndex++){
+        if(input[inputIndex] === vowels[vowelIndex]){
+            resultArray.push(input[inputIndex])
+        }      
+    }
+    if(input[inputIndex] === "e" || input[inputIndex] === "u"){
+        resultArray.push(input[inputIndex])
+    }
 }
 let whaleTranslate = resultArray.join("").toUpperCase();
 console.log(whaleTranslate);
