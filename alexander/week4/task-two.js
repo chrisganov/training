87b06cf7408d43ccd65c0b3b73118da029389852
sunko <sunk0@abv.bg>
2019-09-02 19:32:44 +0300

    v1.1

--- alexander/week4/task-two.js
+++ alexander/week4/task-two.js
@@ -4,7 +4,6 @@ secretMessage.push("to","Program");
 secretMessage.splice(7,1, "right");
 secretMessage.shift();
 secretMessage.unshift("Programming");
-// secretMessage = secretMessage.map(item => 
-//      item.replace('get','know').replace('right','know').replace('the','know').replace('first','know').replace('time','know')
-// ); TODO: Use splice, it does not work, since the words you have replaced have to be replaced with only one word -"know" and not change each word to "know"
+secretMessage.splice(6,5,"know")
+
 console.log(secretMessage.join(" "));
