386eb90f48c4fe34878754d8127a2b566a49b362
ociganov <ociganov@yahoo.com>
2019-09-03 22:21:32 +0300

    Code cleanup

--- daniel/week4/js/task2.js
+++ daniel/week4/js/task2.js
@@ -1,31 +1,9 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-Probably said it once at some point,', 'Ivo', 'Dukov,', 'Learn', 'JavaScript'];
 secretMessage.pop();
 secretMessage.push('to', 'Program');
-secretMessage.splice(7,1,'right'); //TODO:  This is done ok, but I prefer you re-write this line, by selecting it (google how to select a value within an array based on index). Hint: It's not a method and it should be reassigned
+secretMessage.splice(7,1,'right');
 const newArray = Object.assign([], secretMessage, {[7]: 'right'}); // мисля че това искаш да направя!
 secretMessage.shift();
 secretMessage.unshift('Programing');
 secretMessage.splice(6, 5, 'know,'); 
 console.log(secretMessage.join(' '));
-
-
-
-
-/*
-Task2:
-
-You have the following array:
-
-let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-Probably said it once at some point,', 'Ivo', 'Dukov,', 'Learn', 'JavaScript'];
-
-You need to:
-
-1. Remove the last idex using an array method + 
-2. Add the strings "to" and "Program" at the end of the array using an array method +
-3. Change the 7th index of the array with 'right' using an array method +
-4. Remove the first index of the array using an array method + 
-5. Add the string "Programming" at the beggining of the array using an array method +
-6. log the array in the console to see how it looks and if all of the above have been completed. +
-7. Remove the following strings "get, right, the, first, time" and replace them with 'know' using only one array method +
-8. Log in the console a string that has joined all of the strings with a white space using an array method +
-*/
--- daniel/week4/js/task3.js
+++ daniel/week4/js/task3.js
@@ -1,38 +1,31 @@
-//let vowels = ["a", "e", "i", "o", "u"];
-//let resultArray = [];
-//-------------------------
-const vowels = "aeiou";
-let input = 'samurai life above all';
+// const vowels = "aeiou";
+// let input = 'samurai life above all';
+// 
+// 
+// let resultArray = [];
+// for (i = 0; i < input.length; i++) {
+  // if(vowels.indexOf(input[i]) >= 0) resultArray += input[i];
+// }
+// let whaleTranslate = resultArray.toUpperCase();
+// console.log(whaleTranslate);
+// 
 
 
-let resultArray = [];
-for (i = 0; i < input.length; i++) {
-  if(vowels.indexOf(input[i]) >= 0) resultArray += input[i];
-}
-let whaleTranslate = resultArray.toUpperCase();
-console.log(whaleTranslate);
-
-//Доста помощ от нета :(
-/*
-Task3
-
-You have the following string:
+// What it should have been:
 
 const input = 'samurai life above all';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
 
-You need to:
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  for (let vowelsIndex = 0; vowelsIndex < vowels.length; vowelsIndex++) {
+    if (input[inputIndex] === vowels[vowelsIndex]) {
+      resultArray.push(input[inputIndex])
+    }
+  };
+  if (input[inputIndex] === 'e' || input[inputIndex] === 'u') {
+    resultArray.push(input[inputIndex]);
+  }
+}
 
-1. Create a variable called "vowels" which is an array which holds the english vowels (google them if you dont know them. Hint: "y" is not a vowel) + 
-2. Create a variable called resultArray which is an empty array.
-3. Create a loop with the following logic:
-  a. Inside it as a condition:  initiate a variable called inputIndex which is equal to 0, while inputIndex is lower than the input's length, increase inputeIndex by 1 +
-  b. Inside the loop create a second loop:
-    i. Inside it as a condition: initiate a  variable called vowelsIndex which is equal to 0, while vowelsIndex is lower than vowels's length, increase vowelsIndex by 1 +
-    ii. If input with index inputIndex is the same as the vowels with index vowelsIndex, push the input with index inputIndex in resultArray
-  c. If input with index inputIndex is equal to "e" or "u", push the input with index inputIndex into the resultArray 
-4. Create a variable called whaleTranslate which is equal to the resultArray which is joined without any whitespace and are all capital letters 
-5. Log in the console the whaleTranslate varaibel + 
-
-Expected result should be: AUUAIIEEAOEEA
-AUAIIEAOEA
-*/
+console.log(resultArray.join('').toUpperCase())
